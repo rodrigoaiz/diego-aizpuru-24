cf50a59dec5674ba9c49647958017ffb770218ce
Rodrigo Aizpuru <rodrigoaiz@gmail.com>
2024-06-25 22:43:27 -0600

    chore: Update Astro configuration and package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
