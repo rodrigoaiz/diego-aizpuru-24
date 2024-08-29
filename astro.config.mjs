0494a01a1db6fe8dd92ccfd8933802bc231fcbff
Rodrigo Aizpuru <rodrigoaiz@gmail.com>
2024-08-28 22:06:05 -0600

    chore: Update Astro configuration, package dependencies, and editorconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(),
-    icon({
-      include:{
-        bi: [
-          'facebook',
-          'twitter-x',
-          'youtube',
-          'instagram',
-          'box-arrow-in-right',
-        ]
-      }
-    })
-  ]
+  integrations: [tailwind(), icon({
+    include: {
+      bi: ['facebook', 'twitter-x', 'youtube', 'instagram', 'box-arrow-in-right','envelope-paper-heart','file-earmark-arrow-down', 'box-arrow-in-right', 'link-45deg']
+    }
+  }), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  })]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,13 @@ import flowbite from 'flowbite/plugin'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', "./node_modules/flowbite/**/*.js"],
 	theme: {
+		screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
 		extend: {},
 	},
 	plugins: [
